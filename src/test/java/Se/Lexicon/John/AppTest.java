f031eecf2a90eca4e759774e148f26fb6630b1d6
jon0945 <john.gullne@gmail.com>
2019-09-09 12:31:01 +0200

    Unit testing DONE! Assignment complete

--- src/test/java/Se/Lexicon/John/AppTest.java
+++ src/test/java/Se/Lexicon/John/AppTest.java
@@ -1,7 +1,6 @@
 package Se.Lexicon.John;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,9 +12,6 @@ public class AppTest {
     public void setup() {
         testGame.setNumberOfGuesses(0);
         testGame.setSecretWord("Test");
-
-
-
     }
 
     @Test
@@ -51,5 +47,90 @@ public class AppTest {
 
 
     }
+
+    @Test
+    public void testAlreadyGuessedWord() {
+        //Arange
+        testGame.setUserGuess("Pony");
+        testGame.setGuessed("Pony");
+        char[] word = testGame.getSecretWord().toCharArray();
+        int expectedIndex =0;
+        int expectedCounter = 0;
+
+        //Act
+        testGame.evaluateWord(word);
+        int actualIndex = testGame.getIndexofGuessed(testGame.getUserGuess());
+        int actualCounter = testGame.getNumberOfGuesses();
+
+        //Assert
+        assertEquals(actualIndex,expectedIndex);
+        assertEquals(actualCounter,expectedCounter);
+    }
+
+    @Test
+    public void testRightLetter() {
+        //Arange
+        testGame.setUserGuess("e");
+        char[] word = testGame.getSecretWord().toCharArray();
+        char [] guess = new char[word.length];
+        int expectedCounter = 0;
+        char expectedIndex = testGame.getUserGuess().charAt(0);
+
+        //Act
+        testGame.evaluateLetter(guess, word);
+        char actualIndex = word[1];
+        int actualCounter = testGame.getNumberOfGuesses();
+
+        //Assert
+        assertEquals(actualCounter,expectedCounter);
+        assertEquals(actualIndex,expectedIndex);
+    }
+
+    @Test
+    public void testWrongLetter() {
+        //
+        testGame.setUserGuess("a");
+        char[] word = testGame.getSecretWord().toCharArray();
+        char [] guess = new char[word.length];
+        int expectedCounter = 1;
+        char expectedIndex = testGame.getUserGuess().charAt(0);
+
+        //Act
+        testGame.evaluateLetter(guess, word);
+        char actualIndex = word[0];
+        char actualIndex2 = word[1];
+        char actualIndex3 = word[2];
+        char actualIndex4 = word[3];
+        int actualCounter = testGame.getNumberOfGuesses();
+
+        //Assert
+        assertEquals(actualCounter,expectedCounter);
+        assertNotEquals(actualIndex,expectedIndex);
+        assertNotEquals(actualIndex2,expectedIndex);
+        assertNotEquals(actualIndex3,expectedIndex);
+        assertNotEquals(actualIndex4,expectedIndex);
+
+    }
+
+    @Test
+    public void testAlreadyGuessedLetter() {
+        //Arange
+        testGame.setUserGuess("l");
+        testGame.setGuessed("l");
+        char[] word = testGame.getSecretWord().toCharArray();
+        char [] guess = new char[word.length];
+        int expectedIndex =0;
+        int expectedCounter = 1;
+
+        //Act
+        testGame.evaluateLetter(guess,word);
+        int actualIndex = testGame.getIndexofGuessed(testGame.getUserGuess());
+        int actualCounter = testGame.getNumberOfGuesses();
+
+        //Assert
+        assertEquals(actualIndex,expectedIndex);
+        assertEquals(actualCounter,expectedCounter);
+    }
 }
+
 
